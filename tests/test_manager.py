c83f7616b9bcdd8f7fce3fc1f82c0a302cb6cbae
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-04-24 15:56:58 -0400

    Update test_manager.py

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -129,87 +129,87 @@ class TestBotoSesManager:
         assert "AWS_SESSION_TOKEN" not in os.environ
         assert "AWS_REGION" not in os.environ
 
-    @pytest.mark.skipif(
-        is_ci,
-        reason="we don't want to expose real AWS credentials in CI",
-    )
-    def test_cli_context_manager_with_arguments(self):
-        # the bsm object is using the profile "project_boto_session_manager"
-        # which is an IAM user.
-        # however, the aws cli uses different AWS profile
-        self._assert_default_aws_cli_credential_is_different(bsm)
-
-        def get_caller_identity() -> T.Tuple[str, str, str]:
-            args = ["aws", "sts", "get-caller-identity"]
-            response = json.loads(
-                subprocess.run(args, capture_output=True).stdout.decode("utf-8")
-            )
-            user_id, aws_account_id, arn = (
-                response["UserId"],
-                response["Account"],
-                response["Arn"],
-            )
-            return user_id, aws_account_id, arn
-
-        # iam user
-        # the aws cli uses the same AWS profile as the bsm object
-        with bsm.awscli():
-            user_id, aws_account_id, arn = get_caller_identity()
-            assert aws_account_id == bsm.aws_account_id
-            assert arn.endswith(f"user/{TEST_IAM_USER_NAME}")
-            assert "AWS_ACCESS_KEY_ID" not in os.environ
-            assert "AWS_SECRET_ACCESS_KEY" not in os.environ
-            assert "AWS_SESSION_TOKEN" not in os.environ
-            assert "AWS_PROFILE" in os.environ
-
-        self._assert_default_aws_cli_credential_is_different(bsm)
-
-        # assume role
-        bsm_assumed = bsm.assume_role(
-            role_arn=f"arn:aws:iam::{aws_account_id}:role/{TEST_IAM_ROLE_NAME}"
-        )
-        # the aws cli uses the sts session token as the same as the assumed role
-        with bsm_assumed.awscli():
-            user_id, aws_account_id, arn = get_caller_identity()
-            assert aws_account_id == bsm_assumed.aws_account_id
-            assert f"assumed-role/{TEST_IAM_ROLE_NAME}" in arn
-            assert "AWS_ACCESS_KEY_ID" in os.environ
-            assert "AWS_SECRET_ACCESS_KEY" in os.environ
-            assert "AWS_SESSION_TOKEN" in os.environ
-            assert "AWS_PROFILE" not in os.environ
-
-        self._assert_default_aws_cli_credential_is_different(bsm)
-
-        bsm_default = BotoSesManager()
-
-        with bsm_default.awscli():
-            user_id, aws_account_id, arn = get_caller_identity()
-            assert bsm_default.aws_account_id == aws_account_id
-            assert "AWS_ACCESS_KEY_ID" in os.environ
-            assert "AWS_SECRET_ACCESS_KEY" in os.environ
-            if bsm_default.boto_ses.get_credentials().token:
-                assert "AWS_SESSION_TOKEN" in os.environ
-            else:
-                assert "AWS_SESSION_TOKEN" not in os.environ
-            assert "AWS_PROFILE" not in os.environ
-
-    @pytest.mark.skipif(
-        is_ci,
-        reason="we don't want to expose real AWS credentials in CI",
-    )
-    def test_temp_snapshot(self):
-        bsm_default = BotoSesManager()
-        bsm_project = bsm
-        assert bsm_default.aws_account_id != bsm_project.aws_account_id
-
-        with bsm_default.temp_snapshot():
-            with bsm_project.awscli():
-                bsm_new = BotoSesManager()
-                assert bsm_new.aws_account_id == bsm_project.aws_account_id
-                bsm_origin = BotoSesManager.from_snapshot_file()
-                assert bsm_origin.aws_account_id == bsm_default.aws_account_id
-
-        assert PATH_DEFAULT_SNAPSHOT.exists() is False
+    # @pytest.mark.skipif(
+    #     is_ci,
+    #     reason="we don't want to expose real AWS credentials in CI",
+    # )
+    # def test_cli_context_manager_with_arguments(self):
+    #     # the bsm object is using the profile "project_boto_session_manager"
+    #     # which is an IAM user.
+    #     # however, the aws cli uses different AWS profile
+    #     self._assert_default_aws_cli_credential_is_different(bsm)
+    #
+    #     def get_caller_identity() -> T.Tuple[str, str, str]:
+    #         args = ["aws", "sts", "get-caller-identity"]
+    #         response = json.loads(
+    #             subprocess.run(args, capture_output=True).stdout.decode("utf-8")
+    #         )
+    #         user_id, aws_account_id, arn = (
+    #             response["UserId"],
+    #             response["Account"],
+    #             response["Arn"],
+    #         )
+    #         return user_id, aws_account_id, arn
+    #
+    #     # iam user
+    #     # the aws cli uses the same AWS profile as the bsm object
+    #     with bsm.awscli():
+    #         user_id, aws_account_id, arn = get_caller_identity()
+    #         assert aws_account_id == bsm.aws_account_id
+    #         assert arn.endswith(f"user/{TEST_IAM_USER_NAME}")
+    #         assert "AWS_ACCESS_KEY_ID" not in os.environ
+    #         assert "AWS_SECRET_ACCESS_KEY" not in os.environ
+    #         assert "AWS_SESSION_TOKEN" not in os.environ
+    #         assert "AWS_PROFILE" in os.environ
+    #
+    #     self._assert_default_aws_cli_credential_is_different(bsm)
+    #
+    #     # assume role
+    #     bsm_assumed = bsm.assume_role(
+    #         role_arn=f"arn:aws:iam::{aws_account_id}:role/{TEST_IAM_ROLE_NAME}"
+    #     )
+    #     # the aws cli uses the sts session token as the same as the assumed role
+    #     with bsm_assumed.awscli():
+    #         user_id, aws_account_id, arn = get_caller_identity()
+    #         assert aws_account_id == bsm_assumed.aws_account_id
+    #         assert f"assumed-role/{TEST_IAM_ROLE_NAME}" in arn
+    #         assert "AWS_ACCESS_KEY_ID" in os.environ
+    #         assert "AWS_SECRET_ACCESS_KEY" in os.environ
+    #         assert "AWS_SESSION_TOKEN" in os.environ
+    #         assert "AWS_PROFILE" not in os.environ
+    #
+    #     self._assert_default_aws_cli_credential_is_different(bsm)
+    #
+    #     bsm_default = BotoSesManager()
+    #
+    #     with bsm_default.awscli():
+    #         user_id, aws_account_id, arn = get_caller_identity()
+    #         assert bsm_default.aws_account_id == aws_account_id
+    #         assert "AWS_ACCESS_KEY_ID" in os.environ
+    #         assert "AWS_SECRET_ACCESS_KEY" in os.environ
+    #         if bsm_default.boto_ses.get_credentials().token:
+    #             assert "AWS_SESSION_TOKEN" in os.environ
+    #         else:
+    #             assert "AWS_SESSION_TOKEN" not in os.environ
+    #         assert "AWS_PROFILE" not in os.environ
+    #
+    # @pytest.mark.skipif(
+    #     is_ci,
+    #     reason="we don't want to expose real AWS credentials in CI",
+    # )
+    # def test_temp_snapshot(self):
+    #     bsm_default = BotoSesManager()
+    #     bsm_project = bsm
+    #     assert bsm_default.aws_account_id != bsm_project.aws_account_id
+    #
+    #     with bsm_default.temp_snapshot():
+    #         with bsm_project.awscli():
+    #             bsm_new = BotoSesManager()
+    #             assert bsm_new.aws_account_id == bsm_project.aws_account_id
+    #             bsm_origin = BotoSesManager.from_snapshot_file()
+    #             assert bsm_origin.aws_account_id == bsm_default.aws_account_id
+    #
+    #     assert PATH_DEFAULT_SNAPSHOT.exists() is False
 
 
 if __name__ == "__main__":
